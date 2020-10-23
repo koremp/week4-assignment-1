6eae259dc28949a0e4d19eac8706a58d49613b45
koremp <im.koremp@gmail.com>
2020-10-23 20:22:27 +0900

    4-2

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 
 import App from './App';
 
-import store from './store'
+import store from './store';
 
 ReactDOM.render(
   (
